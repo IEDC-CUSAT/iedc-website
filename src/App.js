b3ab7556e80afb89a0c30d768d49bd2b8de088e3
dhanush <dhanushpk50@gmail.com>
2024-03-14 22:26:08 +0530

    about and events changes

--- src/App.js
+++ src/App.js
@@ -1,12 +1,11 @@
+import About from "./components/About";
+import Events from "./components/Events";
+
 function App() {
   return (
     <div className="App">
-      <header className="App-header">
-        <p>
-          Edit <code>src/App.js</code> and save to reload.
-        </p>
-        Learn React
-      </header>
+      <Events />
+      <About />
     </div>
   );
 }
