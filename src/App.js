6dbdfd2442ceef0920568638d09c59cc374f706b
aakash414 <67820270+aakash414@users.noreply.github.com>
2024-03-16 08:18:26 +0530

    gsap

--- src/App.js
+++ src/App.js
@@ -2,12 +2,14 @@ import Events from "./components/Events";
 import About from "./components/About";
 import Header from "./components/Header";
 import Hero from "./components/Hero";
+import ScrollSection from "./components/ScrollSection/ScrollSection";
 
 function App() {
   return (
     <div className="App">
       <Hero />
-      <Header />
+      {/* <Header /> */}
+      <ScrollSection />
       <Events />
       <About />
     </div>
---
+++ src/components/ScrollSection/ScrollSection.js
@@ -0,0 +1,38 @@
+import React from "react";
+import gsap from "gsap";
+import "./ScrollSection.css";
+import { ScrollTrigger } from "gsap/ScrollTrigger";
+
+const ScrollSection = () => {
+  gsap.registerPlugin(ScrollTrigger);
+
+  const textElements = gsap.utils.toArray(".text");
+
+  textElements.forEach((text) => {
+    gsap.to(text, {
+      backgroundSize: "100%",
+      ease: "none",
+      scrollTrigger: {
+        trigger: text,
+        start: "center 80%",
+        end: "center 20%",
+        scrub: true,
+      },
+    });
+  });
+  return (
+    <div className="container">
+      <h1 class="text">
+        IEDC<span>IEDC</span>
+      </h1>
+      <h1 class="text">
+        CUSAT<span>CUSAT</span>
+      </h1>
+      <h1 class="text">
+        2024<span>2024</span>
+      </h1>
+    </div>
+  );
+};
+
+export default ScrollSection;
